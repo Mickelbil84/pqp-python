9e199607cc670522cfe081b9bb670725ef776ed2
Michael Bilevich <michaelmoshe@mail.tau.ac.il>
2024-01-01 19:11:47 +0200

    Added PQP_Model bindings

--- src/pqp/__init__.py
+++ src/pqp/__init__.py
@@ -1,5 +1,7 @@
-from .pqp import *
+from __future__ import annotations
+
+from ._pqp import __doc__, load_model, PQP_Model
 
 __all__ = [
-    
+    "__doc__", "PQP_Model", "load_model"
 ]
--- src/pqp_python.cpp
+++ src/pqp_python.cpp
@@ -2,6 +2,31 @@
 #include <pybind11/stl.h>
 namespace py = pybind11;
 
-PYBIND11_MODULE(pqp, m) {
+#include <vector>
+using std::vector;
+
+#include "PQP.h"
+PQP_Model loadModel(vector<vector<double>> vertices, vector<vector<int>> faces) {
+    PQP_Model model;
+    PQP_REAL p1[3], p2[3], p3[3];
+
+    model.BeginModel();
+    int cnt = 0;
+    for (int i = 0; i < faces.size(); i++) {
+        p1[0] = vertices[faces[i][0]][0]; p1[1] = vertices[faces[i][0]][1]; p1[2] = vertices[faces[i][0]][2];
+        p2[0] = vertices[faces[i][1]][0]; p2[1] = vertices[faces[i][1]][1]; p2[2] = vertices[faces[i][1]][2];
+        p3[0] = vertices[faces[i][2]][0]; p3[1] = vertices[faces[i][2]][1]; p3[2] = vertices[faces[i][2]][2];
+        model.AddTri(p1, p2, p3, cnt++);
+    }
+    model.EndModel();
+    return model;
+}
+
+
+PYBIND11_MODULE(_pqp, m) {
     m.doc() = "Python bindings for the PQP collision detection library.";
+    m.def("load_model", &loadModel, "");
+
+    py::class_<PQP_Model>(m, "PQP_Model", "")
+        .def("mem_usage", &PQP_Model::MemUsage, "");
 }
